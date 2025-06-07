a52d08e9d1d407c38953b97a28c1c64364dab3a8
carlbabiano <cjbabiano5@gmail.com>
2025-06-07 20:23:26 +0800

    login test

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '191a49b9307f6a2cc7f792b3cc6b919c9f3da35c',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '191a49b9307f6a2cc7f792b3cc6b919c9f3da35c',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
